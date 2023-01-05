d907144fa19636a88f6e3804039ad86ca250b430
j.perrochaud <j.perrochaud@student.isartdigital.com>
2023-01-05 16:04:34 +0100

    import grid from master

--- src/grid.hpp
+++ src/grid.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#include "square.hpp"
+#include "entities/square.hpp"
+
+constexpr auto NB_SQUARES_COL = 40;
+constexpr auto NB_SQUARES_ROW = 22;
 
 constexpr auto WIDTH = 1280;
 constexpr auto HEIGHT = 720;
@@ -8,10 +11,11 @@ constexpr auto HEIGHT = 720;
 class Grid
 {
 public:
-	Square square[NB_SQUARES_COL][NB_SQUARES_ROW];
+	Square square[NB_SQUARES_ROW][NB_SQUARES_COL];
 
 	Grid();
 	~Grid() {};
 
 	void drawGrid();
+	float2 getSpawnPoint();
 };
