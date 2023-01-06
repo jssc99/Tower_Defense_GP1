c675f723d6b1bd58260ff023c4fde59cd7e6d2c7
j.perrochaud <j.perrochaud@student.isartdigital.com>
2023-01-06 11:38:35 +0100

    checkpoint update from eole version

---
+++ src/checkpoint.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "types.hpp"
+
+constexpr float2 UP = { 0, -1 };
+constexpr float2 DOWN = { 0, 1 };
+constexpr float2 RIGHT = { 1, 0 };
+constexpr float2 LEFT = { -1, 0 };
+constexpr float2 STOP = { 0, 0 };
+
+class Checkpoint
+{
+public:
+	int value;
+	float2 newDirection;
+
+	Checkpoint() : value(0), newDirection( {0,0} ) {};
+	Checkpoint(int val, float2 dir) : value(val), newDirection(dir) {};
+	~Checkpoint() {};
+};
