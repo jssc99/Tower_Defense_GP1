2c052b642126457fb45e99282e3c29dbc00447d4
Eole TRIER <e.trier@student.isartdigital.com>
2023-01-13 10:18:16 +0100

    enemies +- ok

--- src/entities/enemy.hpp
+++ src/entities/enemy.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
-#include "../game.hpp"
-class Castle;
-class Grid;
+#include "castle.hpp"
+#include "../grid.hpp"
 
 class Enemy : public Entity, public HealthSystem
 {
@@ -19,6 +18,7 @@ public:
 	void setPos(float2 pos);
 	void update(Grid* grid, Castle* castle, int nbEnemies);
 	void draw() override;
+
 private:
 	void mMove(Grid* grid, Castle* castle, int nbEnemies);
 };
